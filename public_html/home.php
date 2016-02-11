88c790663d5b6e1cea80ddce782b5fa860fe7b69
katie.snow.ca <katie.snow.ca@gmail.com>
2016-02-11 00:15:28 -0500

    end 10-FEB-2016

---
+++ public_html/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--Index Page -->
+
+<head>
+	<meta charset="utf=8">
+	<title>DSHome.ca</title>
+	<link href="DSHome.css" rel="stylesheet" />
+</head>
+<body>
+	<h1>Welcome to DSHome.ca</h1>
+	<?php
+	$userID ="";
+  $userID = $_POST['userid'];
+	include_once './panels/menu.php';
+	?>
+
+		
+</body>
+
+</html>
