9d2c11b6ef7dea6ccdef5e67f5a27b553c02363b
katie.snow.ca <katie.snow.ca@gmail.com>
2016-02-29 15:37:57 -0500

    work on pasword reset

--- public_html/forgotPassword.php
+++ public_html/forgotPassword.php
@@ -33,35 +33,45 @@
   $username = getPost('username');
   if($username == "")
   {
-    echo "<form method='post' name='resetPassword' action='forgotPassword.php?' autocomplete='on'>";
+    echo "<form method='post' name='getList' action='forgotPassword.php' autocomplete='on'>";
     echo "User Name: <input type='text' name='username' />";
     echo "<input type='submit' name='submit' value='submit'/ >";
   }
   else
   {
     echo "<p class='debug'>u: $username</p>";
+		if($q1==""){
+			$q1 = mt_rand(1,6);
+			while($q1==$q2 OR $q2 =="")
+			{
+				$q2 = mt_rand(1,6);
+			}
+			while($q1==$q3 OR $q2 ==$q3 OR $q3 =="")
+			{
+				$q3 = mt_rand(1,6);
+			}
+		}
+		$questions = "PasswordQuestion".$q1.",PasswordQuestion".$q2.",PasswordQuestion".$q3;
+		echo "<p class='debug'>$questions</p>";
+		
     include_once './panels/dbConnect.php';
     $dbh = OpenConn();
-    $stmt = $dbh->prepare("SELECT UserID from tUser WHERE UserName=:UserName"); 
-    $stmt->bindParam(":UserName",$username);
-    $results = $stmt->fetch(PDO::FETCH_ASSOC);
-    if(count($results) > 0)
+		$stmt = $dbh->prepare("SELECT UserID,UserName,UPassword,PasswordQuestion1, PasswordQuestion2,PasswordQuestion3,PasswordQuestion4,PasswordQuestion5,PasswordQuestion6,PasswordAnswer1, PasswordAnswer2,PasswordAnswer3,PasswordAnswer4,PasswordAnswer5,PasswordAnswer6 FROM tUser Where UserName=:UserName"); 
+		$stmt->bindParam(":UserName",$username);
+		$stmt->execute();
+		$results = $stmt->fetch(PDO::FETCH_ASSOC);
+	 if(count($results) > 0)
     {
+		 echo "<h2>Password Reset Feature</h2>";
       $UserID=$results['UserID'];
-      echo "<p class='debug'>".count($results)."::User: ".$results['UserID']."//".$results."</p>";
-      if($q1==""){
-      $q1 = mt_rand(1,6);
-      while($q1==$q2 OR $q2 =="")
-      {
-        $q2 = mt_rand(1,6);
-      }
-      while($q1==$q3 OR $q2 ==$q3 OR $q3 =="")
-      {
-        $q3 = mt_rand(1,6);
-      }
-        $query = "SELECT PasswordQuestion$q1,PasswordQuestion$q2,PasswordQuestion$q3 from tUser WHERE UserID=$UserID";
-        echo "<p class='debug'>$query</p>";
-      }
+      echo "<form method='post' name='resetPassword' action='resetPassword.php?$userID' autocomplete='on'>";
+		 	echo " <input type='hidden' name='userid' value='$userID' />";
+		  echo "<br />".$results["PasswordQuestion".$q1].": <input type='text' name='PasswordResponse$q1' /> <input type='hidden' name='PasswordAnswer$q1' value='".$results["PasswordAnswer".$q1]."' />";
+      echo "<br />".$results["PasswordQuestion".$q2].": <input type='text' name='PasswordResponse$q2' /> <input type='hidden' name='PasswordAnswer$q2' value='".$results["PasswordAnswer".$q2]."' />";
+      echo "<br />".$results["PasswordQuestion".$q3].": <input type='text' name='PasswordResponse$q3' /> <input type='hidden' name='PasswordAnswer$q3' value='".$results["PasswordAnswer".$q3]."' />";
+      echo "<br />New Password: <input type='password' name='Pass1' />";
+      echo "<br />Confirm Password: <input type='password' name='Pass2' />";
+		 echo "<input type='submit' name='submit' value='submit'/ >";
     }
     else{
       echo "<p class='error'>User Name not registered</p>";
---
+++ public_html/resetPassword.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!-- 	Purpose: Reset Password by answering questions --> 
+<html>
+<head>
+	<meta charset = "utf-8">
+	<title>Password Reset</title>
+	<link href="DSHome.css" rel="stylesheet" />
+</head>
+<body>
+	<h2>Password Reset</h2>
+<?php
+	include_once './panels/getVariables.php';
+	$userID = getUser();
+	include_once './panels/menu.php';
+  
+?>
+	<div class='box'>
+	<?php
+  $r1 = getPost('PasswordResponse1');
+  $r2 = getPost('PasswordResponse2');
+  $r3 = getPost('PasswordResponse3');
+  $r4 = getPost('PasswordResponse4');
+  $r5 = getPost('PasswordResponse5');
+  $r6 = getPost('PasswordResponse6');
+  $a1 = getPost('PasswordAnswer1');
+  $a2 = getPost('PasswordAnswer2');
+  $a3 = getPost('PasswordAnswer3');
+  $a4 = getPost('PasswordAnswer4');
+  $a5 = getPost('PasswordAnswer5');
+  $a6 = getPost('PasswordAnswer6');
+  $pass1 = getPost('Pass1');
+  $pass2 = getPost('Pass2');
+	if($r1==$a1 and $r2==$a2 and $r3==$a3 and $r4==$a4 and $r5==$a5 and $r6==$a6 and $pass1==$pass2)
+  {
+    echo "<p class='debug'>$pass1 for $userID</p>";
+    include_once './panels/dbConnect.php';
+    $dbh = OpenConn();
+		$stmt = $dbh->prepare("UPDATE tUser SET UPassword=:pwd WHERE UserID=:UserID");
+    $stmt->bindParam(":pwd",$pass1);
+		$stmt->bindParam(":UserID",$userID);
+		$stmt->execute();
+    echo "<form method='post' name='getList' action='index.php' autocomplete='on'>";
+    echo "User Name: <input type='text' name='username' />";
+    echo "<input type='submit' name='submit' value='submit'/ >";
+  }
+  else
+  {
+    echo "<p class='error'>One of your questions was answered incorrectly, please try again or contact support.</p>";
+  }
+  
+  
+?>
+<!--<script language="JavaScript">document.getList.submit();</script>-->
+</body>
+</html>
